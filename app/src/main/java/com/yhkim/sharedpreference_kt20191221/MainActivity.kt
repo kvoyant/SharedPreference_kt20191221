b58fb24045b3d8ed70ce26efa7915738204d12a8
kvoyant <kvoyant@gmail.com>
2019-12-21 11:31:45 +0900

    ID 저장 체크여부도 저장 / 반영

--- app/src/main/java/com/yhkim/sharedpreference_kt20191221/MainActivity.kt
+++ app/src/main/java/com/yhkim/sharedpreference_kt20191221/MainActivity.kt
@@ -2,6 +2,7 @@ package com.yhkim.sharedpreference_kt20191221
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import com.yhkim.sharedpreference_kt20191221.utils.ContextUtil
 import kotlinx.android.synthetic.main.activity_main.*
 
@@ -26,9 +27,21 @@ class MainActivity : BaseActivity() {
             }
         }
 
+//        Q2. 아이디저장 체크박스 체크 여부 (Boolean) 저장
+//        구글링 : 안드로이드 "코틀린" "체크박스" 이벤트 => ""는 절대 검색어 지정하는 방법
+
+        idCheckBox.setOnCheckedChangeListener { buttonView, isChecked ->
+
+//            Log.d("isChecked값",  "${isChecked}") //true, false
+            ContextUtil.setUserIdSave(mContext, isChecked)
+        }
+
     }
 
     override fun setValues() {
+        idEdt.setText(ContextUtil.getUserId(mContext))
+
+        idCheckBox.isChecked = ContextUtil.getUserIdSave(mContext)
     }
 
 }
--- app/src/main/java/com/yhkim/sharedpreference_kt20191221/utils/ContextUtil.kt
+++ app/src/main/java/com/yhkim/sharedpreference_kt20191221/utils/ContextUtil.kt
@@ -12,6 +12,8 @@ class ContextUtil {
         //    사용자의 아이디를 ( 저장-set/로드-get ) 할때 사용하는 항목의 이름을 상수로 저장.
         val USER_ID = "USER_ID"
 
+        val USER_ID_SAVE = "USER_ID_SAVE"
+
         //    userId를 저장하는 기능을 구현한 함수
         fun setUserId(context: Context, inputId: String) {
 //        메모장에서 txt파일을 여는것과 같은 행위
@@ -26,6 +28,19 @@ class ContextUtil {
             val pref = context.getSharedPreferences(prefName, Context.MODE_PRIVATE)
 
             return pref.getString(USER_ID, "")!! //!! => 절대 널이 아님
+        }
+
+        fun setUserIdSave(context: Context, needSave: Boolean) {
+            val pref = context.getSharedPreferences(prefName, Context.MODE_PRIVATE)
+
+            pref.edit().putBoolean(USER_ID_SAVE, needSave).apply()
+        }
+
+        fun getUserIdSave(context: Context) : Boolean {
+            val pref = context.getSharedPreferences(prefName, Context.MODE_PRIVATE)
+
+            return pref.getBoolean(USER_ID_SAVE, false) //저장안되면 false로 저장한다
+
         }
 
     }
